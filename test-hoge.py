fffe1cb09f0c789b4cc99d553fff1ab887a1dce8
Takahiro Kobayashi <koba925@gmail.com>
2019-01-05 17:02:38 +0900

    Added pogepoge

--- test-hoge.py
+++ test-hoge.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python3
 
 import unittest
-from hoge import hogehoge
+from hoge import hogehoge, pogepoge
 
 
 class TestHoge(unittest.TestCase):
     def test_hogehoge(self):
         self.assertEqual(hogehoge(2, 5), 7)
         self.assertEqual(hogehoge(5, 2), 3)
+
+    def test_pogepoge(self):
+        self.assertEqual(pogepoge(3), 6)
 
 
 if __name__ == "__main__":
